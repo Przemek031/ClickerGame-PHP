8301b3935b039e7850387d101254ab062c7317b8
Przemek031 <przemek3253@gmail.com>
2022-05-24 17:59:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,48 @@
+<?php
+
+	session_start();
+	
+	if ((isset($_SESSION['loged'])) && ($_SESSION['loged']==true))
+	{
+		header('Location: game.php');
+		exit();
+	}
+
+?>
 <html>
 <head>
 <meta charset="utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<title>Clicker game PHP</title>
+<title>Clicker Game</title>
 </head>
 <body>
-<?php
-$connect= mysqli_connect('localhost', 'root', '', 'clicker');
-$query="SELECT user,points,perclick FROM data;";
-$results=mysqli_query($connect, $query);
-$tab=mysqli_fetch_row($results);
-$points = $tab[1];
-echo "Player: $tab[0], Have $tab[1] Points and $tab[2] per click";
-?>
-<form action="point.php" method="post">
-</br>Press to get the point</br>
-<input type="submit" name="getpoint" value="Get The Point"/></br></br>
-<?php
-if ($points < 50) 
-{
- echo ' <input type="submit" name="Upgrade1" value="Upgrade 1 (50 Points)" disabled/>';
-}else 
-{
- echo '<input type="submit" name="Upgrade1" value="Upgrade 1 (50 Points)"/>';
-}
-if ($points < 150) 
-{
- echo ' <input type="submit" name="Upgrade2" value="Upgrade 2 (150 Points)" disabled/>';
-}else 
-{
- echo '<input type="submit" name="Upgrade2" value="Upgrade 2 (150 Points)"/>';
-}
-if ($points < 500) 
-{
- echo ' <input type="submit" name="Upgrade3" value="Upgrade 3 (500 Points)" disabled/>';
-}else 
-{
- echo '<input type="submit" name="Upgrade3" value="Upgrade 3 (500 Points)"/>';
-}
-?>
-
-
+<h1>Login</h1>
+<form action="login.php" method="post">
+    Login:<input type="text" name="login" /></br>
+    Password:<input type="password" name="pass" /></br>
+    <input type="submit" value="Login in" name="loginin">
+    <?php
+    if(isset($_SESSION['error']))	echo $_SESSION['error'];
+    ?>
 </form>
-
-<?php
-mysqli_close($connect);
-?>
+<h1>Register</h1>
+<form action="register.php" method="post">
+    Login:<input type="text" name="loginr" /></br>
+    <?php
+    if(isset($_SESSION['e_login'])){	
+    echo $_SESSION['e_login'];echo"</br>";}
+    ?>
+    Email:<input type="text" name="email" /></br>
+    <?php
+    if(isset($_SESSION['e_email'])){	
+    echo $_SESSION['e_email'];echo"</br>";}
+    ?>
+    Password:<input type="password" name="pass1" /></br>
+    Password again:<input type="password" name="pass2" /></br>
+    <?php
+    if(isset($_SESSION['e_pass'])){	
+    echo $_SESSION['e_pass'];echo"</br>";}
+    ?>
+    <input type="submit" value="Register" name="register">
+</form>
 </body>
 </html>
