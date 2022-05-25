8642fcbc27a8a11cfdd1d50ce6ad3385cb1871c3
Przemek031 <przemek3253@gmail.com>
2022-05-25 21:26:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,36 +13,95 @@
 <head>
 <meta charset="utf-8" />
 <title>Clicker Game</title>
+<style>
+body{background: #202522;margin:0;padding:0;}
+.loginpanel
+{
+    width:17%;
+    height:35%;
+    color:#608060;
+    margin:6%;
+    float:left;
+    background:#303532;
+    box-shadow: 5px 5px 10px 5px #407142;
+    border: 1px solid black;
+    border-radius:5px;
+    padding:2%;
+}
+h1
+{
+margin:0 auto;
+color:#a0d0a0;
+font-family: "Lucida Console", "Courier New", monospace;
+}
+h3
+{
+    margin:2% auto;
+}
+input
+{
+width:100%;
+height:33px;
+background:#407142;
+}
+input[type='submit']
+{
+    text-transform:uppercase;
+    cursor:pointer;
+    border-style: solid;
+    border-color: #407142;
+    border-width: 2px;
+    border-radius:5px;
+    background: none;
+    text-align:center;
+    color:#a0d0a0;
+}
+.registerpanel
+{
+    width:17%;
+    height:53%;
+    color:#608060;
+    margin:6%;
+    float:right;
+    background:#303532;
+    box-shadow: 5px 5px 10px 5px #407142;
+    border: 1px solid black;
+    border-radius:5px;
+    padding:2%;
+}
+
+</style>
 </head>
 <body>
-<h1>Login</h1>
-<form action="login.php" method="post">
-    Login:<input type="text" name="login" /></br>
-    Password:<input type="password" name="pass" /></br>
-    <input type="submit" value="Login in" name="loginin">
+
+<div class="Loginpanel">
+<h1>LOGIN</h1></br>
+<form id="loginform" action="login.php" method="post">
+   <center> <h3>Login:</br></h3>
+    <input type="text" name="login" /></br>
+    <h3>Password:</br></h3>
+    <input type="password" name="pass" /></br></br>
+    <input type="submit" value="Login in" name="loginin"></br></br>
     <?php
     if(isset($_SESSION['error']))	echo $_SESSION['error'];
-    ?>
+    ?></center>
 </form>
-<h1>Register</h1>
+</div>
+<div class="registerpanel">
+<h1>REGISTER</h1></br>
 <form action="register.php" method="post">
-    Login:<input type="text" name="loginr" /></br>
+ <center>   <h3>Login:</h3><input type="text" name="loginr" /></br>
+    <h3>Email:</h3><input type="text" name="email" /></br>
+    <h3>Password:</h3><input type="password" name="pass1" /></br>
+    <h3>Password again:</h3><input type="password" name="pass2" /></br>
+</br>
+    <input type="submit" value="Register" name="register"></br></br>
     <?php
-    if(isset($_SESSION['e_login'])){	
-    echo $_SESSION['e_login'];echo"</br>";}
+    if(isset($_SESSION['r_error'])){	
+    echo $_SESSION['r_error'];}
     ?>
-    Email:<input type="text" name="email" /></br>
-    <?php
-    if(isset($_SESSION['e_email'])){	
-    echo $_SESSION['e_email'];echo"</br>";}
-    ?>
-    Password:<input type="password" name="pass1" /></br>
-    Password again:<input type="password" name="pass2" /></br>
-    <?php
-    if(isset($_SESSION['e_pass'])){	
-    echo $_SESSION['e_pass'];echo"</br>";}
-    ?>
-    <input type="submit" value="Register" name="register">
+    </center>
 </form>
+    </div>
 </body>
 </html>
